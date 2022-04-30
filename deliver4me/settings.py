16edb9be016ddfc97b76cb982f9a8362c3006b22
jaywizzy <jaywizzy34@gmail.com>
2022-05-01 00:28:59 +0100

    page title

--- deliver4me/settings.py
+++ deliver4me/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 # STATIC_ROOT = BASE_DIR / "static"
 
